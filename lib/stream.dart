aa00e72020961371266e606c88a5f4f1370c0435
dimasprmana <dimasprmana>
2024-12-22 00:12:59 +0700

    W13: Jawaban Soal 3

--- lib/stream.dart
+++ lib/stream.dart
@@ -8,4 +8,12 @@ class ColorStream {
     Colors.lightBlue,
     Colors.teal,
   ];
+
+  Stream<Color> getColor() async*{
+    yield* Stream.periodic(
+      const Duration(seconds: 1), (int t) {
+        int index = t % colors.length;
+        return colors[index];  
+  });
+  }
 }
